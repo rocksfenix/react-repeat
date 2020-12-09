ba681a90ad62241236f6ea0544b083764338e389
Gerardo Gallegos <puxerop@gmail.com>
2020-12-09 08:22:54 -0600

    adding base colors

---
+++ website/utils/colors.js
@@ -0,0 +1,8 @@
+import * as polished from 'polished';
+
+export const background = '#000';
+export const foreground = '#FFF';
+export const red = '#ff5555';
+
+export const blue = polished.lighten(0.1, '#d1b0dd');
+export const lightGrey = polished.darken(0.06, '#000');
